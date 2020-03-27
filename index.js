31405efff59afb7876abf730ab17d7fdc58bcf88
Obiakor Nkem <62053217+Nkemm@users.noreply.github.com>
2020-03-27 23:10:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+ var name = "Obiakor Nkem";
+ var courses = ["Html","Css","Js"];
+console.log("My name is:"+name);
+console.log("The courses I am currently doing are:"+courses);
+var i;
+for (i=1;i<=200;i++){
+        if(i%2!==0){
+            console.log(i);
+        }
+    }
